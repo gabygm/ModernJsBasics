5ac15084e2b674dc2d233c5f9ab9975fff3386e7
Gaby <paogaby0803@gmail.com>
2023-04-21 12:56:23 -0500

    adds const and let

---
+++ src/conts_let.js
@@ -0,0 +1,10 @@
+const first_name = 'Gaby'
+const last_name = 'Guaman'
+let age = 18
+age = 20
+
+if(true) {
+    const first_name = "Petter"
+    console.log(first_name)
+}
+console.log(first_name)
