844d4eae6d03c708289526739f502e240a211580
Gaby <paogaby0803@gmail.com>
2023-04-21 12:55:12 -0500

    adds array

---
+++ src/arrays.js
@@ -0,0 +1,11 @@
+const persons = []
+const person = {
+    fisrt_name: "Gaby",
+    last_namee: "Guaman",
+    age: 18,
+    address: {
+        city: "Loja"
+    }
+}
+persons.push(person)
+console.log(persons)
